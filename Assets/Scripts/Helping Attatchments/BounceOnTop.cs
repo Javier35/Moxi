7f10feee71fa5d675a4c78b9124f7db026b5f1c4
Javier <35javier35@gmail.com>
2017-03-24 13:23:51 -0600

    Trampoline and crate sprites updated

--- Assets/Scripts/Helping Attatchments/BounceOnTop.cs
+++ Assets/Scripts/Helping Attatchments/BounceOnTop.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 public class BounceOnTop : MonoBehaviour {
 
 	public float bounceForce = 200f;
+	private Animator anim;
+
+	void Start(){
+		anim = GetComponent<Animator> ();
+
+	}
 
 	void OnCollisionEnter2D(Collision2D collision){
 		
@@ -12,10 +18,12 @@ public class BounceOnTop : MonoBehaviour {
 		if (Vector3.Dot (contact.normal, Vector3.down) > 0.5) {
 			var destroyable = collision.gameObject.GetComponent<Destroyable> ();
 			if (destroyable != null) {
+
+				if(anim != null)
+					anim.SetTrigger ("Bounce");
 				
 				if (collision.gameObject.tag == "Player") {
 					var playerBehavior = collision.gameObject.GetComponent<PlatformerCharacter2D> ();
-
 					playerBehavior.DoJump (bounceForce);
 
 				} else {
